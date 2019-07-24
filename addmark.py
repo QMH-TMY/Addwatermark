cca8a77b4a7350c70f65e552cb671400f5821616
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-25 00:26:33 +0800

    modify py

--- addmark.py
+++ addmark.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
-# 为pdf任意页添加指定页水印
-# Date: 2019.06.15
-# Author: Shieber
+# 
+#    Date: 2019/06/15
+#    Author: Shieber, Henry
+#
+#                             APACHE LICENSE
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+#                            Function Description
+#    Touch a watermark onto a pdf file on its any page.
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 import sys
 import PyPDF2
